f5d503a951bdda1ff96610930049d1a65a72ebfa
Jerson Andrés Jaramillo Mejía <jersonandres25@hotmail.com>
2019-08-18 18:05:19 -0500

    add property

---
+++ MyLeasing.Web/Helpers/CombosHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MyLeasing.Web.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyLeasing.Web.Helpers
+{
+    public class CombosHelper : ICombosHelper
+    {
+        private readonly DataContext _dataContext;
+
+        public CombosHelper(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public IEnumerable<SelectListItem> GetComboPropertyTypes()
+        {
+            var list = _dataContext.PropertyTypes.Select(pt => new SelectListItem
+            {
+                Text = pt.Name,
+                Value = $"{pt.Id}"
+            })
+              .OrderBy(pt => pt.Text)
+              .ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "(Select a Property Type...)",
+                Value = "0"
+            });
+            return list;
+        }
+    }
+}
